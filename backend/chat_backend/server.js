cbf03eab81ae58775ea5449857e0d717b29b1cc8
Your Name <hardik.sondhi2022@vitstudent.ac.in>
2025-08-25 21:33:37 +0530

    server

--- backend/chat_backend/server.js
+++ backend/chat_backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000", // Allow frontend
+        origin: "https://collab-space-pink.vercel.app/", // Allow frontend
         methods: ["GET", "POST"]
     }
 });
